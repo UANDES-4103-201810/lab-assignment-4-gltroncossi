3c2dd2fa046456f980ad3084dd2c312587e7b522
GFenzo <gfenzo@miuandes.cl>
2018-04-03 13:44:20 -0300

    minor fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	validates :email, format: { with:/\A[^@\s]+@[^@\s]+\z/}, presence: true, uniqueness: true
   validates :password, length: { in: 8..12 }, presence: true
-  validates :valid_phone, length: { in: 9..12 }, presence: true
+  validates :phone, length: { in: 9..12 }, presence: true
 	has_many :user_tickets
 	has_many :tickets, :through => :user_tickets
 
